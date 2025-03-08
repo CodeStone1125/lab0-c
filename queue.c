93c6190494fc48fbe7530b80171bf393798d71f6
CodeStone1125 <williechu1125@gmail.com>
2025-03-08 17:18:16 +0800

    Import Simplified Merge Sort
    Removed unnecessary attributes to simplify the merge sort function.
    Added a prefix to make the function more identifiable for the original
    sort implementation in the project.
    
    Also added the merge sort to the queue test file.
    
    This change makes the code easier to understand and maintain by reducing
    complexity without affecting the core functionality.
    
    Change-Id: Iae3ca4258b33b32e0670db70ad8287cf063f89bc

--- queue.c
+++ queue.c
@@ -426,3 +426,124 @@ int q_merge(struct list_head *head, bool descend)
     return first->size;
 }
 
+/* Reference lib/list_sort.corder */
+static struct list_head *lx_merge(struct list_head *a, struct list_head *b)
+{
+    struct list_head *head, **tail = &head;
+
+    for (;;) {
+        /* if equal, take 'a' -- important for sort stability */
+        if (strcmp(list_entry(a, element_t, list)->value,
+                   list_entry(b, element_t, list)->value) <= 0) {
+            *tail = a;
+            tail = &a->next;
+            a = a->next;
+            if (!a) {
+                *tail = b;
+                break;
+            }
+        } else {
+            *tail = b;
+            tail = &b->next;
+            b = b->next;
+            if (!b) {
+                *tail = a;
+                break;
+            }
+        }
+    }
+    return head;
+}
+
+static void lx_merge_final(struct list_head *head,
+                           struct list_head *a,
+                           struct list_head *b)
+{
+    struct list_head *tail = head;
+    // u8 count = 0;
+
+    for (;;) {
+        /* if equal, take 'a' -- important for sort stability */
+        if (strcmp(list_entry(a, element_t, list)->value,
+                   list_entry(b, element_t, list)->value) <= 0) {
+            tail->next = a;
+            a->prev = tail;
+            tail = a;
+            a = a->next;
+            if (!a)
+                break;
+        } else {
+            tail->next = b;
+            b->prev = tail;
+            tail = b;
+            b = b->next;
+            if (!b) {
+                b = a;
+                break;
+            }
+        }
+    }
+
+    /* Finish linking remainder of list b on to tail */
+    tail->next = b;
+    do {
+        b->prev = tail;
+        tail = b;
+        b = b->next;
+    } while (b);
+
+    /* And the final links to make a circular doubly-linked list */
+    tail->next = head;
+    head->prev = tail;
+}
+
+void lx_sort(struct list_head *head)
+{
+    struct list_head *list = head->next, *pending = NULL;
+    size_t count = 0; /* Count of pending */
+
+    if (list == head->prev) /* Zero or one elements */
+        return;
+
+    /* Convert to a null-terminated singly-linked list. */
+    head->prev->next = NULL;
+
+    do {
+        size_t bits;
+        struct list_head **tail = &pending;
+
+        /* Find the least-significant clear bit in count */
+        for (bits = count; bits & 1; bits >>= 1)
+            tail = &(*tail)->prev;
+        /* Do the indicated merge */
+        if (bits) {
+            struct list_head *a = *tail, *b = a->prev;
+
+            a = lx_merge(b, a);
+            /* Install the merged result in place of the inputs */
+            a->prev = b->prev;
+            *tail = a;
+        }
+
+        /* Move one element from input list to pending */
+        list->prev = pending;
+        pending = list;
+        list = list->next;
+        pending->next = NULL;
+        count++;
+    } while (list);
+
+    /* End of input; merge together all the pending lists. */
+    list = pending;
+    pending = pending->prev;
+    for (;;) {
+        struct list_head *next = pending->prev;
+
+        if (!next)
+            break;
+        list = lx_merge(pending, list);
+        pending = next;
+    }
+    /* The final merge, rebuilding prev links */
+    lx_merge_final(head, pending, list);
+}
